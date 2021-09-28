7875f76e6ba7159032d924a1bf347264d4b59328
giddie254 <giddiebbest@gmail.com>
2021-09-27 21:37:45 -0700

    url config update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = "https://truthhub254-rest-server.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
